d7830dab34158081e5e28b312d5a6e2c27de74f8
holter <holter@ubuntu>
2016-11-22 21:43:16 +0100

    Initial commit

---
+++ ImageTiler/Tile.bat
@@ -0,0 +1,2 @@
+D:\JBuilder4\jdk1.3\bin\java -classpath .;F:\J2SDK_Forte\forte4j\modules\ext\AbsoluteLayout.jar;.\AbsoluteLayout.jar ImageTiler
+pause
---
+++ ImageTiler/make.bat
@@ -0,0 +1,2 @@
+D:\JBuilder4\jdk1.3\bin\javac -classpath .;F:\J2SDK_Forte\forte4j\modules\ext\AbsoluteLayout.jar;.\AbsoluteLayout.jar ImageTiler.java
+pause
